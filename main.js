79f721b15fa60a84e36e74d12237a2bd1530483b
tdddt <doto1116@ewhain.net>
2021-05-16 13:43:37 +0900

    finish

--- main.js
+++ main.js
@@ -3,19 +3,17 @@ var fs = require('fs');
 var url = require('url'); //url이라는 모듈(nodejs가 가지고 있는 비슷한 기능을 모아놓은 group)을 url이라는 변수를 통해 사용하겠다는 의()
 var qs = require('querystring');
 var template = require('./lib/template.js');
+var path = require('path');
+var sanitizeHtml = require('sanitize-html');
+
 //nodejs로 웹브라우저가 접속이 들어올 때마다 createServer의 callback함수를 호출함
 //request = 요청할 때 웹브라우저가 보낸 정보들
 //response = 응답할 때 우리가 웹브라우저한테 전송할 정보들
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    //변수 url : query string의 값을 가지고 있음
+    var _url = request.url; //변수 url : query string의 값을 가지고 있음
+    var queryData = url.parse(_url, true).query; //queryData를 출력하면 {id: 'HTML'}의 객체 형태로 출력됨 //queryData.id를 출력하면 HTML만 출력
+    var pathname = url.parse(_url, true).pathname; //console.log(url.parse(_url, true)); //주어진 url의 정보를 보여줌.
 
-    var queryData = url.parse(_url, true).query;
-    //queryData를 출력하면 {id: 'HTML'}의 객체 형태로 출력됨
-    //queryData.id를 출력하면 HTML만 출력
-
-    var pathname = url.parse(_url, true).pathname;
-    //console.log(url.parse(_url, true)); //주어진 url의 정보를 보여줌.
 
     if(pathname==='/'){
       if(queryData.id===undefined){ //Home
@@ -32,15 +30,20 @@ var app = http.createServer(function(request,response){
         });
       } else {
         fs.readdir('./data',function(error,filelist){
-          fs.readFile(`data/${queryData.id}`,`utf8`,function(err,description){
+          var filteredId = path.parse(queryData.id).base;
+          fs.readFile(`data/${filteredId}`,`utf8`,function(err,description){
               var title = queryData.id;
+              var sanitizedTitle = sanitizeHtml(title);
+              var sanitizedDescription = sanitizeHtml(description,{
+                allowedTags:['h1']
+              });
               var list = template.list(filelist);
               var html = template.HTML(title, list,
-                `<h2>${title}</h2>${description}`,
+                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                 `<a href="/create">create</a>
-                 <a href="/update?id=${title}">update</a>
+                 <a href="/update?id=${sanitizedTitle}">update</a>
                  <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${title}">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                  </form>`); //삭제는 링크가 아니라 폼으로 구현해야 함
             response.writeHead(200); //200은 파일이 성공적으로 전송되었다는 뜻
@@ -84,7 +87,8 @@ var app = http.createServer(function(request,response){
       });
     } else if(pathname==='/update'){
       fs.readdir('./data',function(error,filelist){
-        fs.readFile(`data/${queryData.id}`,`utf8`,function(err,description){
+        var filteredId = path.parse(queryData.id).base;
+        fs.readFile(`data/${filteredId}`,`utf8`,function(err,description){
             var title = queryData.id;
             var list = template.list(filelist);
             var html = template.HTML(title, list,
@@ -128,7 +132,8 @@ var app = http.createServer(function(request,response){
       request.on('end',function(){
         var post = qs.parse(body);
         var id = post.id;
-        fs.unlink(`data/${id}`,function(error){
+        var filteredId = path.parse(id).base;
+        fs.unlink(`data/${filteredId}`,function(error){
           response.writeHead(302, {Location: `/`});
           response.end();
         })
---
+++ password.js
@@ -0,0 +1,4 @@
+module.exports = {
+  id : 'tdddt',
+  password : '111111'
+}
