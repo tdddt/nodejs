9af2323cf651c84e4ff52a4d356d0cf5e4ecc330
tdddt <doto1116@ewhain.net>
2021-05-16 12:20:25 +0900

    38

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url'); //url이라는 모듈(nodejs가 가지고 있는 비슷한 기능을 모아놓은 group)을 url이라는 변수를 통해 사용하겠다는 의()
+var qs = require('querystring');
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
  return `
  <!doctype html>
  <html>
@@ -13,29 +14,26 @@ function templateHTML(title, list, body){
  <body>
    <h1><a href="/">WEB</a></h1>
    ${list}
+   ${control}
    ${body}
  </body>
  </html>
  `;
 };
 function templateLIST(filelist){
-  /*
-  var list = `<ul>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ul>`;
-  */
-
   var list = '<ul>';
   var i = 0;
-  while(i<filelist.length){
-    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i = i + 1;
   }
   list = list + '</ul>';
+  return list;
 };
 
+//nodejs로 웹브라우저가 접속이 들어올 때마다 createServer의 callback함수를 호출함
+//request = 요청할 때 웹브라우저가 보낸 정보들
+//response = 응답할 때 우리가 웹브라우저한테 전송할 정보들
 var app = http.createServer(function(request,response){
     var _url = request.url;
     //변수 url : query string의 값을 가지고 있음
@@ -54,7 +52,9 @@ var app = http.createServer(function(request,response){
           var title = `Welcome`;
           var description = `Hello~~~! Welcome to Node.js!`;
           var list = templateLIST(filelist);
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a>`);
           response.writeHead(200);
           response.end(template);
         });
@@ -63,7 +63,14 @@ var app = http.createServer(function(request,response){
           fs.readFile(`data/${queryData.id}`,`utf8`,function(err,description){
               var title = queryData.id;
               var list = templateLIST(filelist);
-              var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+              var template = templateHTML(title, list,
+                `<h2>${title}</h2>${description}`,
+                `<a href="/create">create</a>
+                 <a href="/update?id=${title}">update</a>
+                 <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                 </form>`); //삭제는 링크가 아니라 폼으로 구현해야 함
             response.writeHead(200); //200은 파일이 성공적으로 전송되었다는 뜻
             //response.end(fs.readFileSync(__dirname + _url)); //사용자가 접속한 url에 따라 파일들을 읽어주 코드
             //response.end(queryData.id); //바로 위에 코드를 삭제하고 현재 줄의 코드를 입력하면 queryData.id가 웹브라우저에 출력
@@ -71,13 +78,92 @@ var app = http.createServer(function(request,response){
           });
         });
       }
+    } else if(pathname==='/create'){ //입력받는 곳
+      fs.readdir('./data',function(error,filelist){
+        var title = `Web-create`;
+        var list = templateLIST(filelist);
+        var template = templateHTML(title, list,`
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+          `, '');
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname ==='/create_process'){ //입력받는 값에 따라 파일이 디렉토리에 생성되고, 페이지로 이동
+      var body = '';
+      request.on('data', function(data){
+        body = body + data; //정보가 조각조각 들어오게 함
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var title = post.title; //title정보를 받음
+        var description = post.description; //description정보를 받음
+        fs.writeFile(`data/${title}`,description,'utf8',function(err){
+          response.writeHead(302, {Location: `/?id=${title}`}); //302는 페이지를 다른 곳으로 리다이렉션하겠다는 뜻
+          response.end();
+        });
+      });
+    } else if(pathname==='/update'){
+      fs.readdir('./data',function(error,filelist){
+        fs.readFile(`data/${queryData.id}`,`utf8`,function(err,description){
+            var title = queryData.id;
+            var list = templateLIST(filelist);
+            var template = templateHTML(title, list,
+              `<form action="/update_process" method="post">
+                <input type = "hidden" name="id" value="${title}">
+                <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+                <p>
+                  <textarea name="description" placeholder="description">${description}</textarea>
+                </p>
+                <p>
+                  <input type="submit">
+                </p>
+              </form>`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if(pathname==='/update_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data; //정보가 조각조각 들어오게 함
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title; //title정보를 받음
+        var description = post.description; //description정보를 받음
+        fs.rename(`data/${id}`,`data/${title}`,function(error){
+          fs.writeFile(`data/${title}`,description,'utf8',function(err){
+            response.writeHead(302, {Location: `/?id=${title}`}); //302는 페이지를 다른 곳으로 리다이렉션하겠다는 뜻
+            response.end();
+          })
+        });
+      });
+    } else if(pathname==='/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+        body = body + data; //정보가 조각조각 들어오게 함
+      });
+      request.on('end',function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        fs.unlink(`data/${id}`,function(error){
+          response.writeHead(302, {Location: `/`});
+          response.end();
+        })
+      });
     } else {
       response.writeHead(404); //파일을 찾을 수 없을 경우 404를 돌려 줌
       response.end('Not found');
     }
-
-
-
-
 });
 app.listen(3000);
